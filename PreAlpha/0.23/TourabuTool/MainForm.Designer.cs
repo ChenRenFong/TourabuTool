fbc7fb45117d03ea9d47e898f586e527e7484b0c
ChenRenFong <31932763+ChenRenFong@users.noreply.github.com>
2017-09-15 15:59:31 +0800

    TourabuTool

--- PreAlpha/0.23/TourabuTool/MainForm.Designer.cs
+++ PreAlpha/0.23/TourabuTool/MainForm.Designer.cs
@@ -106,7 +106,7 @@
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "MainForm";
-            this.Text = "TourabulTool 0.23";
+            this.Text = "TourabuTool 0.23";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
             this.Load += new System.EventHandler(this.MainForm_Load);
             this.ResumeLayout(false);
