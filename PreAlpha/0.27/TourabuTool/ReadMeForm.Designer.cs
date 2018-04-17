53d9599b0ca79c06255f5aa5280f602843d7d2f2
ChenRenFong <31932763+ChenRenFong@users.noreply.github.com>
2018-04-17 23:49:09 +0800

    TourabuTool

---
+++ PreAlpha/0.27/TourabuTool/ReadMeForm.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace TourabuTool
+{
+    partial class ReadMeForm
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.InformationTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // InformationTextBox
+            // 
+            this.InformationTextBox.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.InformationTextBox.Location = new System.Drawing.Point(13, 13);
+            this.InformationTextBox.MaxLength = 327670;
+            this.InformationTextBox.Multiline = true;
+            this.InformationTextBox.Name = "InformationTextBox";
+            this.InformationTextBox.ReadOnly = true;
+            this.InformationTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.InformationTextBox.Size = new System.Drawing.Size(269, 246);
+            this.InformationTextBox.TabIndex = 0;
+            this.InformationTextBox.TabStop = false;
+            // 
+            // ReadMeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(294, 271);
+            this.Controls.Add(this.InformationTextBox);
+            this.DataBindings.Add(new System.Windows.Forms.Binding("Location", global::TourabuTool.Properties.Settings.Default, "ReadMePosition", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Location = global::TourabuTool.Properties.Settings.Default.ReadMePosition;
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "ReadMeForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ReadMeForm_FormClosing);
+            this.Load += new System.EventHandler(this.ReadMeForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox InformationTextBox;
+
+    }
+}
